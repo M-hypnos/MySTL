c8f32f1e591a5896f09778c0eb8b0880bbd3139b
M-hypnos <46706215+M-hypnos@users.noreply.github.com>
2024-08-05 16:05:47 +0800

    identify添加

--- functors.h
+++ functors.h
@@ -18,6 +18,29 @@ namespace mySTL {
 			return x < y;
 		}
 	};
+	
+	template<class T>
+	struct greater:public binary_function<T,T,bool>
+	{
+		bool operator()(const T& x, const T& y) const {
+			return x > y;
+		}
+	};
+
+	template<class Arg, class Result>
+	struct unary_function
+	{
+		typedef Arg argument_type;
+		typedef Result result_type;
+	};
+
+	template<class T>
+	struct identify :public unary_function<T, T>
+	{
+		const T& operator()(const T& x) const {
+			return x;
+		}
+	};
 }
 
 #endif // !_FUNCTORS_H_
